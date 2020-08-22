9dd673423129e9de0e5d9d5c20ece87762ce2711
mikaere66 <mikaere66@live.com>
2020-08-23 10:40:54 +1200

    Commit from Android Studio

---
+++ Journaler/app/src/main/java/com/michaelrmossman/journaler/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.michaelrmossman.journaler.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.michaelrmossman.journaler.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
